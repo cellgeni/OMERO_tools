6ac1cae69a9bc40b07944c306a7f26b98c654c78
Stanislaw Makarchuk <49611600+StasMakarchuk@users.noreply.github.com>
2025-01-27 16:00:41 +0000

    Update transfer_annotations_different_OMERO_servers.py

--- transfer_annotations_different_OMERO_servers.py
+++ transfer_annotations_different_OMERO_servers.py
@@ -47,7 +47,7 @@ def write_one_roi(omero_username, omero_password, omero_host, polygon, image_id)
     
     #print(f"Connecting to {omero_srv} in port {omero_port}")
     #print(f"Using credentials: UserName={omero_username} Password={len(omero_password)*'*'}")
-    with BlitzGateway(omero_username, omero_password, host=omero_host) as connection:
+    with BlitzGateway(omero_username, omero_password, host=omero_host, secure = True) as connection:
         update_service = connection.getUpdateService()
         result = connection.connect()
         #print(f"Connect: {result}")
@@ -68,10 +68,10 @@ def write_one_roi(omero_username, omero_password, omero_host, polygon, image_id)
         #roi.textValue = rstring("test-Polygon")
         update_service.saveObject(roi)
 
-def collect_ROIs_from_OMERO(omero_username, omero_password, omero_host, omero_image_id):
+def collect_ROIs_from_OMERO(omero_username, omero_password, omero_host, omero_port, omero_image_id):
     ROIs = []
     log.info(f"Connecting to OMERO at {omero_host}")
-    with BlitzGateway(omero_username, omero_password, host=omero_host, port=4064, secure=True) as conn:
+    with BlitzGateway(omero_username, omero_password, host=omero_host, port=omero_port, secure=True) as conn:
         # search image
         log.info(f"Looking for ImageId {omero_image_id}")
         conn.SERVICE_OPTS.setOmeroGroup('-1')
@@ -95,29 +95,54 @@ def collect_ROIs_from_OMERO(omero_username, omero_password, omero_host, omero_im
 
             primary_shape = roi.getPrimaryShape()
             name = primary_shape.getTextValue().val
+            stroke_color = roi.getPrimaryShape().getStrokeColor()
+            fill_color = roi.getPrimaryShape().getFillColor()
+            stroke_width = roi.getPrimaryShape().getStrokeWidth()
+            stroke_dash =  roi.getPrimaryShape().getStrokeDashArray()
             #if ROI name is empty - renamed to "Non_labelled", makes sure first letter is capital, if additional csv provided - unified all different ROIs name into one
             #print(name)
             #, separates x and y and  space separates points
+            
+            
+           
             try:
-                points = [(lambda xy : list(map(float,xy.split(","))))(xy) for xy in primary_shape.getPoints().val.split(" ")]
-                ROIs.append({
-                "name": name,
-                "points": points
-                })
+                points = []
+                for xy in primary_shape.getPoints().val.split(" "):
+                    if len(xy)>3:
+                        points.append([float(xy.split(",")[0]), float(xy.split(",")[1])])
+
+                    #points = [(lambda xy : list(map(float,xy.split(","))))(xy) for xy in primary_shape.getPoints().val.split(" ")]
+                    ROIs.append({
+                    "name": name,
+                    "points": points,
+                    "stroke_color": stroke_color,
+                    "stroke_width": stroke_width,
+                    "fill_color": fill_color,
+                    "stroke_dash": stroke_dash,    
+                    })
             except:
                 if primary_shape.__class__.__name__ == 'RectangleI':
                     points = get_corners_rectangle(primary_shape)
                     ROIs.append({
                     "name": name,
-                    "points": points
+                    "points": points,
+                    "stroke_color": stroke_color,
+                    "stroke_width": stroke_width,
+                    "fill_color": fill_color,
+                    "stroke_dash": stroke_dash,  
                     })
                 else:
                     pass
-            
+                
             log.debug(f"Found ROI id={roi.id.val} name='{name}' type={primary_shape.__class__.__name__}")
 
     log.info(f"Found {len(ROIs)} ROIs in total")
     return ROIs, image
+
+def get_corners_rectangle(primary_shape):
+    x0 = primary_shape.getX()._val; y0 = primary_shape.getY()._val
+    w = primary_shape.getWidth()._val; h = primary_shape.getHeight()._val
+    return [(x0, y0), (x0+w,y0), (x0+w,y0+h), (x0,y0+h)]
 
 def array_to_string(ar):
     ss = ""
@@ -133,11 +158,20 @@ def get_OMERO_credentials():
     omero_password = getpass("Password$")
     return omero_username, omero_password
 
-def contstruct_polygon(roi):
+def contstruct_polygon(roi, stroke_width_default = 5):
+    #print(roi)
     polygon = omero.model.PolygonI()
-    polygon.fillColor = rint(rgba_to_int(255, 0, 255, 50))
-    polygon.strokeColor = rint(rgba_to_int(255, 255, 0))
-    polygon.strokeWidth = omero.model.LengthI(10, UnitsLength.PIXEL)
+    polygon.fillColor = roi['fill_color']
+    polygon.strokeColor = roi['stroke_color']
+    try:
+        polygon.strokeDashArray = roi['stroke_dash']
+    except:
+        pass
+    if isinstance(roi['stroke_width'], (int, float)):
+        polygon.strokeWidth = roi['stroke_width']
+    else:
+        polygon.strokeWidth = omero.model.LengthI(stroke_width_default, UnitsLength.PIXEL)
+        
     points = array_to_string(roi['points'])
     polygon.points = rstring(points)
     polygon.textValue = rstring(roi['name'])
@@ -148,17 +182,19 @@ def main(csv_path):
     #initialization
     print('Please provide credentials for OMERO server 1:')
     omero_host_1 = "wsi-omero-prod-02.internal.sanger.ac.uk"
+    omero_port_1 = 4064
     omero_username_1, omero_password_1 = get_OMERO_credentials()
     
     print('Please provide credentials for OMERO server 2:')
-    omero_host_2 = "wsi-omero-prod-02.internal.sanger.ac.uk"
+    omero_host_2 = "wsi-omero-prod-02.internal.sanger.ac.uk" #"dtomero.sdsc.edu"
     omero_username_2, omero_password_2 = get_OMERO_credentials()
     
     for i in range(table_input.shape[0]):
-        print(i)
+        
         omero_id_in = table_input['omero_id_1'][i]
         omero_id_out = table_input['omero_id_2'][i]
-        ROIs, image = collect_ROIs_from_OMERO(omero_username_1, omero_password_1, omero_host_1, omero_id_in)
+        ROIs, image = collect_ROIs_from_OMERO(omero_username_1, omero_password_1, omero_host_1, omero_port_1, omero_id_in)
+        print(str(i) + '/' + str(table_input.shape[0]))
         for roi in ROIs:
             pol = contstruct_polygon(roi)
             write_one_roi(omero_username_2, omero_password_2, omero_host_2, pol, omero_id_out)
